e06de36f76eff52acdde0bd91abbfc8e32e56792
EduardaBezerra <eduardaf.bezerra@outlook.com>
2022-05-03 06:25:34 -0300

    1. Mexendo nos estilos 2. Criadas classes 3. botões refeitos

--- project/src/App.js
+++ project/src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import Calculator from './Components/Calculator';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1> Calculator </h1>
+      {/* <h1> Calculator </h1> */}
 
-      <Calculator/>
+      <div className="calculator-grid">
+
+          <div className="output">
+              <div className="previous-operand"></div>
+              <div className="current-operand"></div>
+            
+            <button className="span-two"> AC </button>
+            <button> DEL </button>
+            <button> / </button>
+            <button> 7 </button>
+            <button> 8 </button>
+            <button> 9 </button>
+            <button> * </button>
+            <button> 4 </button>
+            <button> 5 </button>
+            <button> 6 </button>
+            <button> + </button>
+            <button> 1 </button>
+            <button> 2 </button>
+            <button> 3 </button>
+            <button> - </button>
+            <button> . </button>
+            <button> 0 </button>
+            <button className="span-two"> = </button>
+
+          </div>
+      </div>
     </div>
   );
 }
